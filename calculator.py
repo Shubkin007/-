f3b6a178a3138e7d1d149415703b5b5d905e09f9
ivans <ivan.shubin.1989@mail.ru>
2023-10-03 23:31:45 +0300

    practic calculator.py

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def main():
+    try:
+        num1 = float(input("Введите первое число: "))
+        operator = input("Введите арифметический оператор (+, -, *, /): ")
+        num2 = float(input("Введите второе число: "))
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            if num2 == 0:
+                print("Ошибка: Деление на ноль невозможно!")
+                return
+            else:
+                result = num1 / num2
+        else:
+            print("Ошибка: Неверный оператор! Введите один из: +, -, *, /")
+            return
+
+        print(f"Результат: {num1} {operator} {num2} = {result}")
+
+    except ValueError:
+        print("Ошибка: Некорректный ввод. Пожалуйста, введите числа и арифметический оператор корректно.")
+
+if __name__ == "__main__":
+    main()
